040f303b4a623926d0ad65bc2b83b4dac73ff2d0
Danial-abd <danial.aburrahman@gmail.com>
2025-02-27 07:19:58 -0800

    fix style, and changes on checkbox

--- app.js
+++ app.js
@@ -3,48 +3,55 @@ let totalTask = 0;
 // document.getElementById("numbers").innerHTML = ' 0/' + totalTask;
 
 const tambahTask = () => {
-    const taskInput = document.getElementById("taskInput")
-    const text = taskInput.value.trim()
-    
-    if (text) {
-        tasks.push({
-            text: text,
-            completed: false
-        });
-    }
-    
-    updateTaskList()
-    totalTask = tasks.length;
-    document.getElementById("numbers").innerHTML =  '0 / ' + totalTask;
+  const taskInput = document.getElementById("taskInput");
+  const text = taskInput.value.trim();
+
+  if (text) {
+    tasks.push({
+      text: text,
+      completed: false,
+    });
+    taskInput.value = "";
+    updateTaskList();
+  }
+  totalTask = tasks.length;
+  document.getElementById("numbers").innerHTML = "0 / " + totalTask;
 };
 
-const updateTaskList = ()=> {
-    const taskList = document.getElementById("task-list")
-    taskList.innerHTML = ''
+const toggleTaskCompleted = (index) => {
+  tasks[index].completed = !tasks[index].completed;
+  updateTaskList()
+};
 
-    tasks.forEach((task, index )=> {
-        const listItem = document.createElement('li');
+const updateTaskList = () => {
+  const taskList = document.getElementById("task-list");
+  taskList.innerHTML = "";
 
-        listItem.innerHTML = `
+  tasks.forEach((task, index) => {
+    const listItem = document.createElement("li");
+
+    listItem.innerHTML = `
         <div class="taskItem">
-            <div class="task ${task.completed ? 'completed' : ''}">
-                <input type="checkbox" class="checkbox" ${task.completed ? 'checked' : ''}/>
-                <p>${task.text}</p>
-            </div>
-            <div class="icons">
-                <img src="./asset/img/edit.png" onClick="editTask(${index})" />
-                <img src="./asset/img/bin.png" onClick="deleteTask(${index})" />
-            </div>
+        <div class="task ${task.completed ? "completed" : ""}">
+        <input type="checkbox" class="checkbox" ${
+          task.completed ? "checked" : ""
+        }/>
+        <p>${task.text}</p>
+        </div>
+        <div class="icons">
+        <img src="./asset/img/edit.png" onClick="editTask(${index})" />
+        <img src="./asset/img/bin.png" onClick="deleteTask(${index})" />
+        </div>
         </div>
         `;
-        
-        listItem.addEventListener('change', ()=> toggleTaskCompleted(index));
+    
+        listItem.addEventListener("change", () => toggleTaskCompleted(index));
         taskList.append(listItem);
-    });
+  });
 };
 
-document.getElementById("taskBaru").addEventListener("click", function(e) { 
-    e.preventDefault()
+document.getElementById("taskBaru").addEventListener("click", function (e) {
+  e.preventDefault();
 
-    tambahTask()
- });
+  tambahTask();
+});
