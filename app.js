482b4d2294f959aa26d44f134323c69e9538b7a0
danial.abd <danial.aburrahman@gmail.com>
2025-03-01 06:13:45 +0800

    edit and delete button function, local storage, make progress bar works

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const storedTasks = JSON.parse(localStorage.getItem('tasks'))
+
+  if(storedTasks){
+    storedTasks.forEach((task)=> tasks.push(task))
+    updateTaskList();
+    updateStats();
+  }
+})
+
 let tasks = [];
-let totalTask = 0;
 // document.getElementById("numbers").innerHTML = ' 0/' + totalTask;
+
+const saveTasks = () => {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
 
 const tambahTask = () => {
   const taskInput = document.getElementById("taskInput");
@@ -13,14 +26,46 @@ const tambahTask = () => {
     });
     taskInput.value = "";
     updateTaskList();
+    updateStats();
+    saveTasks();
   }
-  totalTask = tasks.length;
-  document.getElementById("numbers").innerHTML = "0 / " + totalTask;
 };
 
 const toggleTaskCompleted = (index) => {
   tasks[index].completed = !tasks[index].completed;
-  updateTaskList()
+  updateTaskList();
+  updateStats();
+  saveTasks();
+};
+
+const deleteTask = (index) => {
+  tasks.splice(index, 1);
+  updateTaskList();
+  updateStats();
+  saveTasks();
+};
+
+const editTask = (index) => {
+  const taskInput = document.getElementById("taskInput");
+  taskInput.value = tasks[index].text;
+
+  tasks.splice(index, 1);
+  updateTaskList();
+  updateStats();
+  saveTasks();
+};
+
+const updateStats = () => {
+  const completedTask = tasks.filter((task) => task.completed).length;
+  const totalTasks = tasks.length;
+  const progress = (completedTask / totalTasks) * 100;
+  const progresBar = document.getElementById("progress");
+
+  progresBar.style.width = `${progress}%`;
+
+  document.getElementById(
+    "numbers"
+  ).innerText = `${completedTask} / ${totalTasks}`;
 };
 
 const updateTaskList = () => {
@@ -44,9 +89,9 @@ const updateTaskList = () => {
         </div>
         </div>
         `;
-    
-        listItem.addEventListener("change", () => toggleTaskCompleted(index));
-        taskList.append(listItem);
+
+    listItem.addEventListener("change", () => toggleTaskCompleted(index));
+    taskList.append(listItem);
   });
 };
 
